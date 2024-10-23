d7133650e9dff48db38a5df324e27f085e578d01
emanuel-garcia-5 <emanuelgarcia209@gmail.com>
2024-10-22 22:19:02 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors({
+    origin:'*'
+}))
+const http = require('http')
+const server = http.createServer(app)
+const {Server} = require("socket.io")
+const io = new Server(server, {cors:{origin:'*'}})
+
+io.on('connection', (socket) => {
+    console.log('Se ha conectado un usuario')
+    socket.on('arduinoData',
+
+        (data) => {
+            console.log(data)
+            socket.broadcast.emit('arduino', data)
+        }
+    )
+})
+
+server.listen(8080, '0.0.0.0',()=>{
+    console.log('Servidor running')
+})
